6e8b4dee3e3be972c45e242f30c52b5e2e273efc
Yocky Andrea <54408235+yocky12k@users.noreply.github.com>
2019-12-26 13:45:20 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,22 @@
+<?php
+
+// emailmu
+$email = "addxd2866@gmail.com";
+
+// referl_id mu
+$referl_id = "403866";
+
+// points terserah mau di isi brp tapi sesuai list spin di bawah
+/* list spin
+10
+20
+30
+40
+50
+60
+70
+80 
+*/
+
+$points = "80";
+
